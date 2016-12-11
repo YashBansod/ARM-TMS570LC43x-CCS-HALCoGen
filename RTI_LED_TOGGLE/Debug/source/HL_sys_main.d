8f46076e043007b711ad9aee470e41aeccdaf036
Yash Bansod <theyashbansod@gmail.com>
2016-12-11 12:28:04 +0530

    The following project demonstrates the toggling of user LEDs on the Hercules TMS570LC43x Development board using CCS. The LEDs are toggled with a delay of 1000 milli-second using the RTI (Real Time Interrupt).
    The following code can be edited to generate a code for
    timer interrupt operations.

---
+++ RTI_LED_TOGGLE/Debug/source/HL_sys_main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+source/HL_sys_main.obj: ../source/HL_sys_main.c
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_sys_core.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_sys_common.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_hal_stdtypes.h
+source/HL_sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_esm.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_esm.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_rti.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_rti.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_gio.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_gio.h
+
+../source/HL_sys_main.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_sys_core.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_esm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_esm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_rti.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_rti.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_gio.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_LED_TOGGLE/include/HL_reg_gio.h: 
