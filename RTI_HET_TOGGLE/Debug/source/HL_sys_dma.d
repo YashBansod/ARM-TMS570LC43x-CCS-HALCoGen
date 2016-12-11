9be5c40739ec5c206525e842c494e0dc621a89c4
Yash Bansod <theyashbansod@gmail.com>
2016-12-11 15:54:25 +0530

    The following code demonstrates the toggling of hetPORT1 pin 1 & 2 with a duration of 100 milli-seconds using the RTI (Real Time Interrupts). The code is written using CCS and HALCoGen.
    This code can be edited to operate the hetPORT 1 & 2 with real time
    interrupts.

---
+++ RTI_HET_TOGGLE/Debug/source/HL_sys_dma.d
@@ -0,0 +1,21 @@
+# FIXED
+
+source/HL_sys_dma.obj: ../source/HL_sys_dma.c
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_dma.h
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_reg_dma.h
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_common.h
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_hal_stdtypes.h
+source/HL_sys_dma.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_sys_dma.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_vim.h
+source/HL_sys_dma.obj: C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_reg_vim.h
+
+../source/HL_sys_dma.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_dma.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_reg_dma.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_sys_vim.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/RTI_HET_TOGGLE/include/HL_reg_vim.h: 
