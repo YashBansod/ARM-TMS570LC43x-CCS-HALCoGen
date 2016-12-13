6aa8269a84e5f0b9f990cda916ef4565928c12a2
Yash Bansod <theyashbansod@gmail.com>
2016-12-13 12:37:38 +0530

    This program recieves ASCII characters from a UART enabled device and stores it in a variable. The variable is also echoed back to the user. Also initially a prompt is transmitted to the user for entering input.
    This can be modified to recieve data from sensors sending value through
    UART, or control microcontroller through UART

---
+++ SCI_UART_RX/Debug/source/HL_sci.d
@@ -0,0 +1,23 @@
+# FIXED
+
+source/HL_sci.obj: ../source/HL_sci.c
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sci.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_sci.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_common.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_hal_stdtypes.h
+source/HL_sci.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_sci.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_gio.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_vim.h
+source/HL_sci.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_vim.h
+
+../source/HL_sci.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sci.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_sci.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_gio.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_vim.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_vim.h: 
---
+++ SCI_UART_RX/Debug/source/HL_sys_main.d
@@ -0,0 +1,19 @@
+# FIXED
+
+source/HL_sys_main.obj: ../source/HL_sys_main.c
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sci.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_sci.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_common.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_hal_stdtypes.h
+source/HL_sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_sys_main.obj: C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_gio.h
+
+../source/HL_sys_main.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sci.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_sci.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/SCI_UART_RX/include/HL_reg_gio.h: 
