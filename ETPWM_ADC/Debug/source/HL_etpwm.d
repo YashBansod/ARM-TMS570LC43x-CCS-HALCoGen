cad54846e05a5246e2ee3fae15f11e89c3f4b919
Yash Bansod <theyashbansod@gmail.com>
2017-01-21 21:38:52 +0530

    This project controls the ETPWM duty with an ADC input. This project was made using CCS and HALCoGen. The project can be edited to get a varying duty PWM from the ETPWM module. The input to vary the PWM can be any ADC input. With further modifications it can be made to accept inputs through the UART channel.

---
+++ ETPWM_ADC/Debug/source/HL_etpwm.d
@@ -0,0 +1,25 @@
+# FIXED
+
+source/HL_etpwm.obj: ../source/HL_etpwm.c
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_etpwm.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_etpwm.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_common.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_hal_stdtypes.h
+source/HL_etpwm.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_etpwm.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_pinmux.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_pinmux.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_vim.h
+source/HL_etpwm.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_vim.h
+
+../source/HL_etpwm.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_etpwm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_etpwm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_pinmux.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_pinmux.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_vim.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_vim.h: 
---
+++ ETPWM_ADC/Debug/source/HL_notification.d
@@ -0,0 +1,33 @@
+# FIXED
+
+source/HL_notification.obj: ../source/HL_notification.c
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_esm.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_esm.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_common.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_hal_stdtypes.h
+source/HL_notification.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+source/HL_notification.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_adc.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_adc.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_etpwm.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_etpwm.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_epc.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_epc.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_dma.h
+source/HL_notification.obj: C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_dma.h
+
+../source/HL_notification.c: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_esm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_esm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_common.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_adc.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_adc.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_etpwm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_etpwm.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_epc.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_epc.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_sys_dma.h: 
+C:/Users/Yash\ Bansod/Documents/Hercules/ETPWM_ADC/include/HL_reg_dma.h: 
